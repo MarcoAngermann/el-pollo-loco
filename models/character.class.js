3518b261747cf1b66b28e435dc00e8f037a955c2
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-06-19 09:38:22 +0200

    requestAnimationFrame / enemys / clouds

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,5 @@
 class Character extends MovableObject {
-
+    x = 20;
     constructor() {
         super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
     }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,6 +2,6 @@ class Chicken extends MovableObject {
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/2_w.png');
-        
+        this.x = 200 + Math.random() * 500;
 }
 }
