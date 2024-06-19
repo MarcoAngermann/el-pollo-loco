927e21736d99e9b314a64408d9a90d57913799f8
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-06-19 15:04:39 +0200

    animate chicken (enemies)

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,8 +2,25 @@ class Chicken extends MovableObject {
 height = 80;
 width = 80;
 y = 355;
+IMAGES_WALKING_CHICKEN = [
+    'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+    'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+    'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+];
+
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/2_w.png');
+        this.loadImages(this.IMAGES_WALKING_CHICKEN);
         this.x = 200 + Math.random() * 500;
+        this.animate();
+    }
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING_CHICKEN.length;
+            let path = this.IMAGES_WALKING_CHICKEN[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        },200);
+     
     }
 }
