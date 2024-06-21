abbf6fc972a3a61e8c06b27f20071872832583df
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-06-21 09:18:08 +0200

    walking sound

--- models/character.class.js
+++ models/character.class.js
@@ -13,6 +13,7 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-26.png'
     ];
     world;
+    walking_sound = new Audio('audio/running.mp3');
     constructor() {
         super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
         this.loadImages(this.IMAGES_WALKING);
@@ -21,15 +22,18 @@ class Character extends MovableObject {
 
     animate() {
         setInterval(() => {
+            this.walking_sound.pause();
             if((this.world.keyboard.RIGHT || this.world.keyboard.D) && this.x < this.world.level.level_end_x){
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.walking_sound.play();
             }
         },1000 / 60);
         setInterval(() => {
-            if(this.world.keyboard.LEFT && this.x > 0){
+            if((this.world.keyboard.LEFT || this.world.keyboard.A) && this.x > 0){
                 this.x -= this.speed;
                 this.otherDirection = true;
+                this.walking_sound.play();
             }
             this.world.camera_x = -this.x + 100;
         },1000 / 60);
